78285497d0fa8f3b0c8617cb9ed059241d4d1b00
Aybek Sultanov <aybek-sultanov@inbox.ru>
2022-01-16 23:40:04 +0600

    add new components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Movies from './components/Movies';
+import Home from './components/Home';
+import Admin from './components/Admin';
 
 export default function App() {
   return (
@@ -44,16 +47,4 @@ export default function App() {
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function Movies() {
-  return <h2>Movies</h2>;
-}
-
-function Admin() {
-  return <h2>Admin</h2>;
 }
