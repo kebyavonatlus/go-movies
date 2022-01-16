fe360e941a61d4f5c1ac245877c1fc500f4f4750
Aybek Sultanov <aybek-sultanov@inbox.ru>
2022-01-17 01:02:28 +0600

    add new component (onemovie)

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import {
 import Movies from './components/Movies';
 import Home from './components/Home';
 import Admin from './components/Admin';
-import Categories from './components/Categories'
+import Categories from './components/Categories';
+import OneMovie from './components/OneMovie';
 
 export default function App() {
   return (
@@ -41,9 +42,7 @@ export default function App() {
           </div>
           <div className='col-md-10'>
             <Switch>
-              <Route path="/movies/:id" >
-                <Movie />
-              </Route>
+              <Route path="/movies/:id" component={OneMovie} />
               <Route path="/movies" >
                 <Movies />
               </Route>
