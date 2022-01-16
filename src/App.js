aa10ee36a55483918a8aedafe7c0ef97dcefc78c
Aybek Sultanov <aybek-sultanov@inbox.ru>
2022-01-16 23:29:17 +0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className='container'>
-      <div className='row'>
-        <h1 className='mt-3'>
-          Go Watch a Movie!
-        </h1>
-        <hr className='mb-3'></hr>
-      </div>
-      <div className='row'>
-        <div className='col-md-2'>
-          <ul className='list-group'>
-            <li className='list-group-item'>
-              <a href='/'>Home</a>
-            </li>
-            <li className='list-group-item'>
-              <a href='/movies'>Movies</a>
-            </li>
-            <li className='list-group-item'>
-              <a href='/admin'>Manage Catalogue</a>
-            </li>
-          </ul>
+    <Router>
+      <div className='container'>
+        <div className='row'>
+          <h1 className='mt-3'>
+            Go Watch a Movie!
+          </h1>
+          <hr className='mb-3'></hr>
         </div>
-        <div className='col-md-10'>
+        <div className='row'>
+          <div className='col-md-2'>
+            <ul className='list-group'>
+              <li className='list-group-item'>
+                <Link to="/">Home</Link>
+              </li>
+              <li className='list-group-item'>
+                <Link to="/movies">Movies</Link>
+              </li>
+              <li className='list-group-item'>
+                <Link to="/admin">Manage Catalogue</Link>
+              </li>
+            </ul>
+          </div>
+          <div className='col-md-10'>
+            <Routes>
+              <Route path="/Movies" element={<Movies />}>
+              </Route>
+              <Route path="/admin" element={<Admin />}>
+              </Route>
+              <Route path="/" element={<Home />}>
+              </Route>
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Movies() {
+  return <h2>Movies</h2>;
+}
+
+function Admin() {
+  return <h2>Admin</h2>;
+}
