b9431dbd02d0e817b69daa042f57c89d8372d508
Aybek Sultanov <aybek-sultanov@inbox.ru>
2022-01-21 23:59:14 +0600

    divide for component input select textarea

---
+++ src/components/form-component/Select.js
@@ -0,0 +1,30 @@
+const Select = (props) => {
+    return (
+        <div className="mb-3">
+            <label htmlFor={props.name} className="form-label">
+                {" "}
+                {props.title} {" "}
+            </label>
+            <select className="form-select"
+                name={props.name}
+                value={props.value}
+                onChange={props.handleChange}
+            >
+                <option value="">{props.placeholder}</option>
+                {props.options.map((o) => {
+                    return (
+                        <option className="form-select"
+                            key={o.id}
+                            value={o.id}
+                            label={o.value}
+                            key={o.id} >
+                            {o.value}
+                        </option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default Select;
